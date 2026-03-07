74a5d9f72788cd089bf148c7b12df707a024da13
gspaer <gspaer1903@gmail.com>
2026-03-08 00:21:46 +0300

    дз1

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +14,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val cardSearch = findViewById<androidx.cardview.widget.CardView>(R.id.cardSearch)
+        cardSearch.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
+
+
+        val cardMedia = findViewById<androidx.cardview.widget.CardView>(R.id.cardMedia)
+        cardMedia.setOnClickListener {
+            val displayIntent = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
+
+
+        val cardSett = findViewById<androidx.cardview.widget.CardView>(R.id.cardSett)
+        cardSett.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
     }
 }
+
